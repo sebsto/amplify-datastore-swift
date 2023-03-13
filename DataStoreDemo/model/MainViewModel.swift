80de08493c1d7d417c96c50935c2dc41edb85a91
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2023-03-13 08:55:55 +0100

    first attempt  not working

--- DataStoreDemo/model/MainViewModel.swift
+++ DataStoreDemo/model/MainViewModel.swift
@@ -36,28 +36,19 @@ extension MainView {
             // when we did not load podcast for this category yet
             print("Loading podcast for \(category)")
             self.podcastState[category] = .loading
-            
-            //            // the callback version
-            //            self.backend.loadPodcast(for: category) { result in
-            //                switch(result) {
-            //                case .success(let podcastData):
-            //                    print("Podcast callback yielded \(podcastData.count) results")
-            //                    let result = self.convertDataToModel(podcastData: podcastData)
-            //                    self.podcastState[category] = .dataAvailable(result)
-            //                case .failure(let error):
-            //                    self.podcastState[category] = .error(error)
-            //                }
-            //            }
 
-            // the asyncstream version
+            // using an asyncstream
             do {
-                for try await data in self.backend.loadPodcast(for: category) {
+                print("==== ENTERING PODCAST LOOP =====")
+                for try await snapshot in self.backend.loadPodcast(for: category) {
+                    let data = snapshot.items
                     print("==== PODCAST LOOP yielded \(data.count) results")
                     let result = convertDataToModel(podcastData :data)
                     self.podcastState[category] = .dataAvailable(result)
                 }
                 print("==== EXITED PODCAST LOOP =====")
             } catch {
+                print("==== ERROR DURING PODCAST LOOP =====")
                 self.podcastState[category] = .error(error)
             }
         }
@@ -77,22 +68,10 @@ extension MainView {
             
             episodeState[podcast.id] = . loading
             
-            //         // the callback version
-            //        self.backend.loadEpisodes(for: podcast) { result in
-            //            switch(result) {
-            //            case .success(let data):
-            //                print("Episode callback yielded \(data.count) values")
-            //                let result = self.convertDataToModel(episodeData: data)
-            //                self.episodeState[podcast.id] = .dataAvailable(result)
-            //            case .failure(let error):
-            //                self.episodeState[podcast.id] = .error(error)
-            //
-            //            }
-            //        }
-            
-            // the AsyncStream version
+            // using an AsyncStream
             do {
-                for try await data in self.backend.loadEpisodes(for: podcast) {
+                for try await snapshot in self.backend.loadEpisodes(for: podcast) {
+                    let data = snapshot.items
                     print("==== EPISODE LOOP yielded \(data.count) results")
                     let result = convertDataToModel(episodeData: data)
                     self.episodeState[podcast.id] = .dataAvailable(result)
